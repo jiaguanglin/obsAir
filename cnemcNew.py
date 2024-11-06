9658dc3dabe43d40b45ac25dc98b01a295169e16
jiaguanglin <myelin9166@foxmail.com>
2024-11-06 10:23:43 +0800

    Update cnemcNew.py

--- cnemcNew.py
+++ cnemcNew.py
@@ -54,8 +54,8 @@ if __name__ == "__main__":
 
     # 将每小时数据保存为 csv 文件
     timestamp = df['timepoint'].unique()[-1][:13]
-    #daily_folder = Path('Archive')/timestamp[:10]
-    daily_folder = Path('Archive')
+    #daily_folder = Path('archive')/timestamp[:10]
+    daily_folder = Path('archive')
     daily_folder.mkdir(parents=True, exist_ok=True)
     # 如果已经有过该文件只需要追加此刻获取的即可
     if (daily_folder/(timestamp+'.csv')).exists():
